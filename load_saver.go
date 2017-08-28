a76414573d988df157c9f7dbd0c392d82c8d2f4f
Benedikt Böhm <bb@xnull.de>
2017-08-28 14:26:39 +0000

    fix indefinite lag after save error

--- load_saver.go
+++ load_saver.go
@@ -1,23 +1,56 @@
 package groupprocessor
 
-import "github.com/Shopify/sarama"
+import (
+	"fmt"
+
+	"github.com/Shopify/sarama"
+	"github.com/bobziuchkovski/cue"
+)
 
 type LoadSaver interface {
-	Load(*sarama.ConsumerMessage) (Processable, error)
+	Load(*sarama.ConsumerMessage) Processable
 	Save(Processable) error
+	Done(Processable) bool
+	Fail(Processable, error) bool
 }
 
 type DefaultLoadSaver struct {
+	Name string
+	Log  cue.Logger
+}
+
+func (ls *DefaultLoadSaver) setDefaults() {
+	if ls.Name == "" {
+		ls.Name = fmt.Sprintf("DefaultLoadSaver:%p", ls)
+	}
+
+	if ls.Log == nil {
+		ls.Log = cue.NewLogger(ls.Name)
+	}
 }
 
 func (ls *DefaultLoadSaver) Load(
 	msg *sarama.ConsumerMessage,
-) (Processable, error) {
+) Processable {
 	return &DefaultProcessable{
 		msg: msg,
-	}, nil
+	}
 }
 
 func (ls *DefaultLoadSaver) Save(p Processable) error {
 	return nil
+}
+
+func (ls *DefaultLoadSaver) Done(p Processable) bool {
+	// do nothing and remove message from inflight
+	// override this method to track metrics etc
+	return true
+}
+
+func (ls *DefaultLoadSaver) Fail(p Processable, err error) bool {
+	// log error and remove message from inflight
+	// override this method to implement a custom retry logic
+	ls.setDefaults()
+	ls.Log.Error(err, "failed to process message")
+	return true
 }
