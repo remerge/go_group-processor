bd94a7241d25ab0efe940a717eaf3406adefd246
Alexander Dorofeev <aka.spin@gmail.com>
2019-05-04 02:43:33 +0200

    use one backoff per op

--- processor.go
+++ processor.go
@@ -1,28 +1,8 @@
 package groupprocessor
 
 import (
-	"math/rand"
-	"time"
-
-	"github.com/cenkalti/backoff"
 	"github.com/remerge/cue"
 )
-
-type RandomBackoff struct {
-	Max int64
-}
-
-func NewRandomBackoff(max time.Duration) backoff.BackOff {
-	return &RandomBackoff{
-		Max: int64(max),
-	}
-}
-
-func (b *RandomBackoff) NextBackOff() time.Duration {
-	return time.Duration(rand.Int63n(b.Max))
-}
-
-func (*RandomBackoff) Reset() {}
 
 type Processor interface {
 	Messages() chan interface{}
@@ -37,12 +17,10 @@ type Processor interface {
 
 type DefaultProcessor struct {
 	ID  string
-	ebo backoff.BackOff
 	log cue.Logger
 }
 
 func (p *DefaultProcessor) New() (err error) {
-	p.ebo = NewRandomBackoff(time.Millisecond * 500)
 	p.log = cue.NewLogger(p.ID)
 	return nil
 }
@@ -59,16 +37,7 @@ func (p *DefaultProcessor) OnProcessed(processable Processable) {
 	}).Debug("processed message")
 }
 
-func (p *DefaultProcessor) OnRetry(processable Processable) {
-	backoff := p.ebo.NextBackOff()
-
-	p.log.WithFields(cue.Fields{
-		"value":   processable.Value(),
-		"backoff": backoff,
-	}).Warn("retrying failed message")
-
-	time.Sleep(backoff)
-}
+func (p *DefaultProcessor) OnRetry(_ Processable) {}
 
 func (p *DefaultProcessor) OnSkip(processable Processable, err error) {
 	// nolint: errcheck
