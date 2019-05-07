0c0554f1bb81c0f5d28cba20ca89894b8c8064a7
Alexander Dorofeev <aka.spin@gmail.com>
2019-05-07 16:49:02 +0200

    comment-out legacy code

--- sarama_processor.go
+++ sarama_processor.go
@@ -3,12 +3,9 @@ package groupprocessor
 import (
 	"fmt"
 	"hash/fnv"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/Shopify/sarama"
-	"github.com/remerge/cue"
 	wp "github.com/remerge/go-worker_pool"
 	rand "github.com/remerge/go-xorshift"
 )
@@ -61,14 +58,14 @@ type SaramaProcessor struct {
 
 	sync.RWMutex
 
-	client   sarama.Client
-	consumer sarama.ConsumerGroup
+	// client   sarama.Client
+	// consumer sarama.ConsumerGroup
 
 	messages    chan interface{}
 	messagePool *wp.Pool
 
-	loadedOffsets   map[int32]int64
-	inFlightOffsets map[int32]map[int64]*sarama.ConsumerMessage
+	// loadedOffsets   map[int32]int64
+	// inFlightOffsets map[int32]map[int64]*sarama.ConsumerMessage
 }
 
 type SaramaProcessorConfig struct {
@@ -98,72 +95,73 @@ func (p *SaramaProcessor) init() (err error) {
 		return err
 	}
 
-	if p.Config == nil {
-		p.Config = sarama.NewConfig()
-		p.Config.Consumer.MaxProcessingTime = 30 * time.Second
-		p.Config.Consumer.Offsets.Initial = sarama.OffsetOldest
-		p.Config.Group.Return.Notifications = true
-	}
-
-	p.Config.ClientID = p.ID
-	p.Config.Version = sarama.V0_10_0_0
-
-	p.client, err = sarama.NewClient(
-		strings.Split(p.Brokers, ","),
-		p.Config,
-	)
-
-	if err != nil {
-		return err
-	}
-
-	group := fmt.Sprintf("%s.%s.%d", p.Name, p.Topic, p.GroupGen)
-	p.consumer, err = sarama.NewConsumerGroupFromClient(
-		p.client,
-		group,
-		[]string{p.Topic},
-	)
-
-	if err != nil {
-		return err
-	}
-
-	p.messages = make(chan interface{})
-	p.messagePool = wp.NewPool(p.ID+".messages", 1, p.messageWorker)
-	p.messagePool.Run()
-
-	p.loadedOffsets = make(map[int32]int64)
-	p.inFlightOffsets = make(map[int32]map[int64]*sarama.ConsumerMessage)
+	// if p.Config == nil {
+	// 	p.Config = sarama.NewConfig()
+	// 	p.Config.Consumer.MaxProcessingTime = 30 * time.Second
+	// 	p.Config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	// 	p.Config.Group.Return.Notifications = true
+	// }
+	//
+	// p.Config.ClientID = p.ID
+	// p.Config.Version = sarama.V0_10_0_0
+	//
+	// p.client, err = sarama.NewClient(
+	// 	strings.Split(p.Brokers, ","),
+	// 	p.Config,
+	// )
+	//
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// group := fmt.Sprintf("%s.%s.%d", p.Name, p.Topic, p.GroupGen)
+	// p.consumer, err = sarama.NewConsumerGroupFromClient(
+	// 	p.client,
+	// 	group,
+	// 	[]string{p.Topic},
+	// )
+	//
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// p.messages = make(chan interface{})
+	// p.messagePool = wp.NewPool(p.ID+".messages", 1, p.messageWorker)
+	// p.messagePool.Run()
+	//
+	// p.loadedOffsets = make(map[int32]int64)
+	// p.inFlightOffsets = make(map[int32]map[int64]*sarama.ConsumerMessage)
 
 	return nil
 }
 
 func (p *SaramaProcessor) messageWorker(w *wp.Worker) {
-	for {
-		select {
-		case <-w.Closer():
-			w.Done()
-			return
-		case msg, ok := <-p.consumer.Messages():
-			if ok {
-				p.messages <- msg
-			} else {
-				w.Done()
-				return
-			}
-		case n, ok := <-p.consumer.Notifications():
-			if !ok {
-				break
-			}
-			p.log.WithFields(cue.Fields{
-				"added":    n.Claimed,
-				"current":  n.Current,
-				"released": n.Released,
-			}).Infof("group rebalance")
-		}
-	}
+	// for {
+	// 	select {
+	// 	case <-w.Closer():
+	// 		w.Done()
+	// 		return
+	// 	case msg, ok := <-p.consumer.Messages():
+	// 		if ok {
+	// 			p.messages <- msg
+	// 		} else {
+	// 			w.Done()
+	// 			return
+	// 		}
+	// 	case n, ok := <-p.consumer.Notifications():
+	// 		if !ok {
+	// 			break
+	// 		}
+	// 		p.log.WithFields(cue.Fields{
+	// 			"added":    n.Claimed,
+	// 			"current":  n.Current,
+	// 			"released": n.Released,
+	// 		}).Infof("group rebalance")
+	// 	}
+	// }
 }
 
+// Messages returns
 func (p *SaramaProcessor) Messages() chan interface{} {
 	return p.messages
 }
@@ -173,27 +171,27 @@ func (p *SaramaProcessor) Wait() {
 }
 
 func (p *SaramaProcessor) OnLoad(processable Processable) {
-	msg := processable.Value().(*sarama.ConsumerMessage)
+	// msg := processable.Value().(*sarama.ConsumerMessage)
 
-	p.Lock()
-	defer p.Unlock()
-
-	if p.loadedOffsets[msg.Partition] < msg.Offset {
-		p.loadedOffsets[msg.Partition] = msg.Offset
-	}
-
-	if p.inFlightOffsets[msg.Partition] == nil {
-		p.inFlightOffsets[msg.Partition] = make(
-			map[int64]*sarama.ConsumerMessage,
-		)
-	}
-
-	p.inFlightOffsets[msg.Partition][msg.Offset] = msg
+	// p.Lock()
+	// defer p.Unlock()
+	//
+	// if p.loadedOffsets[msg.Partition] < msg.Offset {
+	// 	p.loadedOffsets[msg.Partition] = msg.Offset
+	// }
+	//
+	// if p.inFlightOffsets[msg.Partition] == nil {
+	// 	p.inFlightOffsets[msg.Partition] = make(
+	// 		map[int64]*sarama.ConsumerMessage,
+	// 	)
+	// }
+	//
+	// p.inFlightOffsets[msg.Partition][msg.Offset] = msg
 }
 
 func (p *SaramaProcessor) OnProcessed(processable Processable) {
-	p.processableFinished(processable)
-	p.DefaultProcessor.OnProcessed(processable)
+	// p.processableFinished(processable)
+	// p.DefaultProcessor.OnProcessed(processable)
 }
 
 func (p *SaramaProcessor) OnSkip(processable Processable, err error) {
@@ -204,37 +202,36 @@ func (p *SaramaProcessor) OnSkip(processable Processable, err error) {
 // processableFinished is called either once the processable is successfully
 // processed (OnProcessed) or when all of the retries were exhausted (OnSkip).
 func (p *SaramaProcessor) processableFinished(processable Processable) {
-	msg := processable.Value().(*sarama.ConsumerMessage)
-
-	p.Lock()
-	defer p.Unlock()
-
-	delete(p.inFlightOffsets[msg.Partition], msg.Offset)
+	// msg := processable.Value().(*sarama.ConsumerMessage)
+	//
+	// p.Lock()
+	// defer p.Unlock()
+	//
+	// delete(p.inFlightOffsets[msg.Partition], msg.Offset)
 }
 
 func (p *SaramaProcessor) OnTrack() {
-	p.RLock()
-	defer p.RUnlock()
-
-	offsets := make(map[int32]int64)
-
-	// when all messages have been processed p.inFlightOffsets is empty, so we
-	// use the latest loaded offset for commit instead
-	for partition, offset := range p.loadedOffsets {
-		offsets[partition] = offset
-	}
-
-	for partition, offsetMap := range p.inFlightOffsets {
-		for offset := range offsetMap {
-			if offsets[partition] == 0 || offset < offsets[partition] {
-				offsets[partition] = offset
-			}
-		}
-	}
-
-	for partition, offset := range offsets {
-		p.consumer.MarkOffset(p.Topic, partition, offset+1, "")
-	}
+	// p.RLock()
+	// defer p.RUnlock()
+	//
+	// offsets := make(map[int32]int64)
+	//
+	// // when all messages have been processed p.inFlightOffsets is empty, so we
+	// // use the latest loaded offset for commit instead
+	// for partition, offset := range p.loadedOffsets {
+	// 	offsets[partition] = offset
+	// }
+	//
+	// for partition, offsetMap := range p.inFlightOffsets {
+	// 	for offset := range offsetMap {
+	// 		if offsets[partition] == 0 || offset < offsets[partition] {
+	// 			offsets[partition] = offset
+	// 		}
+	// 	}
+	// }
+	//
+	// for partition, offset := range offsets {
+	// 	p.consumer.MarkOffset(p.Topic, partition, offset+1, "")
 }
 
 // Close all pools, save offsets and close Kafka-connections
@@ -244,11 +241,11 @@ func (p *SaramaProcessor) Close() {
 
 	p.log.Info("consumer group shutdown")
 	// nolint: errcheck
-	p.log.Error(p.consumer.Close(), "consumer group shutdown failed")
+	// p.log.Error(p.consumer.Close(), "consumer group shutdown failed")
 
 	p.log.Info("kafka client shutdown")
 	// nolint: errcheck
-	p.log.Error(p.client.Close(), "kafka client shutdown failed")
+	// p.log.Error(p.client.Close(), "kafka client shutdown failed")
 
 	p.log.Info("message pool shutdown")
 	p.messagePool.Close()
