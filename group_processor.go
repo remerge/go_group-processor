3603be32b213577970cccaa2744e9aac2b5dc442
Alexander Dorofeev <aka.spin@gmail.com>
2019-11-07 15:25:09 +0100

    close pools after processor

--- group_processor.go
+++ group_processor.go
@@ -189,10 +189,8 @@ func (gp *GroupProcessor) Run() {
 	}()
 	go func() {
 		gp.exitErr = gp.Processor.Wait()
-		if gp.exitErr != nil {
-			gp.loadPool.Close()
-			gp.savePool.Close()
-		}
+		gp.loadPool.Close()
+		gp.savePool.Close()
 		gp.wg.Done()
 	}()
 }
@@ -206,12 +204,6 @@ func (gp *GroupProcessor) Wait() error {
 func (gp *GroupProcessor) Close() {
 	gp.log.Info("processor shutdown")
 	gp.Processor.Close()
-
-	gp.log.Info("load pool shutdown")
-	gp.loadPool.Close()
-
-	gp.log.Info("save pool shutdown")
-	gp.savePool.Close()
-
+	
 	gp.log.Infof("group processor shutdown done")
 }
