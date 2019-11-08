458f17f4b0b1904c9c8ac8a9fe4fb9c27da0a00e
Alexander Dorofeev <aka.spin@gmail.com>
2019-11-08 01:18:30 +0100

    use timeout for pools closing

--- group_processor.go
+++ group_processor.go
@@ -177,21 +177,23 @@ func (gp *GroupProcessor) saveWorker(w *wp.Worker) {
 
 // Run the GroupProcessor consisting of trackPool, savePool and loadPool
 func (gp *GroupProcessor) Run() {
-	gp.wg.Add(3)
+	gp.wg.Add(1)
 	gp.savePool.Run()
 	gp.loadPool.Run()
-	go func() {
-		gp.savePool.Wait()
-		gp.wg.Done()
-	}()
-	go func() {
-		gp.loadPool.Wait()
-		gp.wg.Done()
-	}()
+
 	go func() {
 		gp.exitErr = gp.Processor.Wait()
-		gp.loadPool.Close()
-		gp.savePool.Close()
+		poolsClosedCh := make(chan struct{})
+		go func() {
+			gp.savePool.Wait()
+			gp.loadPool.Wait()
+			close(poolsClosedCh)
+		}()
+
+		select {
+		case <-poolsClosedCh:
+		case <-time.After(time.Second * 30):
+		}
 		gp.wg.Done()
 	}()
 }
